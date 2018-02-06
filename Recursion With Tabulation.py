2cd9d48e750f1d59d7b13bd9a46f6a1072a8fdea
Moses Saffa Gbondo <gbondos@berea.edu>
2018-02-06 14:31:10 -0500

    Update Recursion With Tabulation.py

--- Recursion With Tabulation.py
+++ Recursion With Tabulation.py
@@ -1,8 +1,9 @@
 
 def fibonacci(n):
-  t = [0]*n
-  t[0]  = 0
-  t[1] = 1
-  for i in range(2, n):
-    fibonacci(i) = t[i-1] +t[i-2]
+  table = [0]*(n + 1)
+  table[0]  = 0
+  table[1] = 1
+  for i in range(2, n+1):
+    table[i] = t[i-1] +t[i-2]
+  return table[n] 
     
